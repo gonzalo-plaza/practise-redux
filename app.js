97fb8e89c3f987925ee2a7583227ee70dbcaac2d
gonzalo-plaza <plazaruedag@gmail.com>
2023-01-28 15:22:07 +0100

    [ADD] Add more stores and more actions

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const createStore = require('redux').createStore;
+const {createStore, combineReducers} = require('redux');
 
 // Actions
 const BUY_CAR = 'BUY_CAR';
@@ -6,42 +6,67 @@ const BUY_MOTORBIKE = 'BUY_MOTORBIKE';
 const RETURN_CAR = 'RETURN_CAR';
 const RETURN_MOTORBIKE = 'RETURN_MOTORBIKE';
 
+const BUY_CAR_MIRROR = 'BUY_CAR_MIRROR';
+const BUY_MOTORBIKE_MIRROR = 'BUY_MOTORBIKE_MIRROR';
+const RETURN_CAR_MIRROR = 'RETURN_CAR_MIRROR';
+const RETURN_MOTORBIKE_MIRROR = 'RETURN_MOTORBIKE_MIRROR';
+
 /**
  * Action to buy a product in the shop
- * @param {number} qty Quantity of products to buy
+ * @param {number} qty Quantity of vehicles to buy
  * @param {string} type Type of buy action
  * @returns redux action
  */
-const buy_product = (qty, type) => {
+const product_action = (qty, type) => {
     return {
         type: type,
         payload: qty
     }
 }
-
-/**
- * Action to return a product in the shop
- * @param {number} qty Quantity of products to return 
- * @param {string} type Type of return action
- * @returns redux action
- */
-const return_product = (qty, type) => {
-    return {
-        type: type,
-        payload: qty
-    }
-}
-
-
 
 // Reducers
-const default_products_sate = {
+const default_vehicles_sate = {
     car: 10,
     motorbike: 5
 }
 
+const default_accesories_state = {
+    car_mirror: 5,
+    motorbike_mirror: 10
+}
 
-const games_reducer = (state = default_products_sate, action) => {
+
+const accesories_reducer = (state = default_accesories_state, action) => {
+    switch(action.type){
+        case BUY_CAR_MIRROR: {
+            return {
+                ...state,
+                car_mirror: state.car_mirror - action.payload
+            }
+        }
+        case BUY_MOTORBIKE_MIRROR: {
+            return {
+                ...state,
+                motorbike_mirror: state.motorbike_mirror - action.payload
+            }
+        }
+        case RETURN_CAR_MIRROR: {
+            return {
+                ...state,
+                car_mirror: state.car_mirror + action.payload
+            }
+        }
+        case RETURN_MOTORBIKE_MIRROR: {
+            return {
+                ...state,
+                motorbike_mirror: state.motorbike_mirror + action.payload
+            }
+        }
+        default: return state;
+    }
+}
+
+const vehicles_reducer = (state = default_vehicles_sate, action) => {
     switch(action.type){
         case BUY_CAR: {
             return {
@@ -70,13 +95,19 @@ const games_reducer = (state = default_products_sate, action) => {
         default: return state;
     }
 }
+
+const rootReducers = combineReducers({
+    vehicles_reducer,
+    accesories_reducer
+});
+
+
 // Store
-const store = createStore(games_reducer);
+const store = createStore(rootReducers);
 console.log('Initial state: ', store.getState());
 store.subscribe(() => {
     console.log('State change: ', store.getState())
 });
-store.dispatch(buy_product(3, BUY_CAR));
-store.dispatch(return_product(1, RETURN_CAR));
-store.dispatch(buy_product(2, BUY_MOTORBIKE));
-store.dispatch(return_product(3, RETURN_MOTORBIKE));
+
+store.dispatch(product_action(3, BUY_CAR));
+store.dispatch(product_action(3, RETURN_CAR_MIRROR));
